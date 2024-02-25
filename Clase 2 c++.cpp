89007d9b0527aa3eb00b842a70aa30d0b6321a8b
MaxReinhard866 <160535321+MaxReinhard866@users.noreply.github.com>
2024-02-24 21:15:03 -0600

    Tarea 2
    estructuras de control if, if...else y switch

---
+++ Clase 2 c++.cpp
@@ -0,0 +1,42 @@
+//MAX REINHARD 
+#include <iostream>
+using namespace std;
+main(){
+
+//numero es positivo > 0 o negativo < 0
+
+	int num=0;
+	cout<<"ingrese el numero:";
+	cin>>num;
+	if(num>0){
+	
+		//muestra a aca cuando la funcion es verdadera 
+		cout<<"positivo"<<endl;
+}
+
+else{
+	
+	cout<<"negativo"<<endl;
+}
+system("pause");
+		
+	}
+			
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
